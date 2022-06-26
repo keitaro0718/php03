88853596db6bcba2f1346dce9bb6d439f4ff8eba
keitaro0718 <sugik0718@gmail.com>
2022-06-26 16:21:26 +0900

    17行目

--- bm_update.php
+++ bm_update.php
@@ -14,7 +14,7 @@ $pdo = db_conn();      //DB接続関数
 $sql = "update gs_bm_table set name=:name, url=:url, comment=:comment, where id=:id";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':name',  $name,   PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
-$stmt->bindValue(':url', $email,  PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':url', $url,  PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':comment',$comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':id',$id,  PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
 $status = $stmt->execute(); //実行
